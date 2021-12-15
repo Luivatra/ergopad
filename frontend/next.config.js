bfb0047af8458af9bd86fc609662d8c9e5639dfd
esot321c <86281023+esot321c@users.noreply.github.com>
2021-12-14 16:13:48 -0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    // CHANGE FOR PRODUCTION VERSION
     API_URL: 'https://ergopad.io/api',
     FORM_EMAIL: 'ergopad.marketing@gmail.com',
-    // API_URL: 'http://3.87.194.195:8000/api',
-    // FORM_EMAIL: 'test@testemail.xyz',
   },
 }
